861a4d08896574b7259df1022ca23becac31c36e
Rsiham <128421471+Rsiham@users.noreply.github.com>
2026-06-18 15:49:37 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,16 @@
+public class Main {
 
+    public static void main(String[] args) {
+
+        System.out.println("🎬 Movie Recommender");
+
+        System.out.println("Utilisateur : Siham");
+
+        System.out.println("Films notés :");
+        System.out.println("Titanic : 5");
+        System.out.println("Avatar : 4");
+
+        System.out.println("\nRecommandation :");
+        System.out.println("Matrix : 4.7 ⭐");
+    }
+}
